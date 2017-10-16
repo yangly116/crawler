3eb355782d7ea582ab9b3964e165b4af23dba7e2
Administrator <Administrator@HWW9OZ97NS5PKTO>
2017-10-16 23:40:26 +0800

    进行编码转换

--- testPy/baike_spider/html_outputer.py
+++ testPy/baike_spider/html_outputer.py
@@ -3,6 +3,7 @@ Created on Oct 11, 2017
 
 @author: Administrator
 '''
+import urllib
 
 
 class HtmlOutputer(object):
@@ -17,16 +18,19 @@ class HtmlOutputer(object):
 
     
     def output_html(self):
-        fout = open('output.html','w')
+        fout = open('output.html','w',encoding='utf-8')
         fout.write("<html>")
+        fout.write("<meta charset=\"utf-8\" /> ")
         fout.write("<body>")
         fout.write("<table>")
         #ascii
         for data in self.datas:
+            pp = str(data['summary'].encode('utf-8'),'utf-8')
+            print('pp:'+pp)
             fout.write("<tr>")
-            fout.write("<td>%s</td>" % data['url'])
-            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
-            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("<td>%s</td>" % str(data['url'].encode('utf-8'),'utf-8'))
+            fout.write("<td>%s</td>" % str(data['title'].encode('utf-8'),'utf-8'))
+            fout.write("<td>%s</td>" % pp)
             fout.write("</tr>")
         fout.write("</table>")
         fout.write("</body>")
--- testPy/baike_spider/spider_main.py
+++ testPy/baike_spider/spider_main.py
@@ -25,7 +25,7 @@ class SpiderMain(object):
                 new_urls,new_data = self.parser.parse(new_url,html_cont)
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
-                if count == 50:
+                if count == 1000:
                     break
                 count = count + 1
             except:
